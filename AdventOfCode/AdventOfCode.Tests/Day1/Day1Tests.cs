847c536be5c74dc3db357fc81fa651424d7b047e
maxime.andre <maxime.andr@gmail.com>
2022-12-01 10:20:11 +0100

    day1

---
+++ AdventOfCode/AdventOfCode.Tests/Day1/Day1Tests.cs
@@ -0,0 +1,76 @@
+﻿using System.IO;
+using System.Reflection;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day1Tests
+    {
+        private ITestOutputHelper _output;
+        public Day1Tests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Test1Part1()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day1/Resources/test.txt";
+            Day1 day = new Day1();
+
+            // Act
+            int result = day.Compute(filePath);
+
+            // Assert
+            result.Should().Be(24000);
+        }
+
+
+        [Fact]
+        public void SolutionPart1()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day1/Resources/input.txt";
+            Day1 day = new Day1();
+
+            // Act
+            int result = day.Compute(filePath);
+
+            // Result
+            _output.WriteLine(result.ToString());
+        }
+
+        [Fact]
+        public void Test1Part2()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day1/Resources/test.txt";
+            Day1 day = new Day1();
+
+            // Act
+            int result = day.Compute(filePath, 3);
+
+            // Assert
+            result.Should().Be(45000);
+        }
+
+
+        [Fact]
+        public void SolutionPart2()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day1/Resources/input.txt";
+            Day1 day = new Day1();
+
+            // Act
+            int result = day.Compute(filePath, 3);
+
+            // Result
+            _output.WriteLine(result.ToString());
+        }
+    }
+}
+
---
+++ AdventOfCode/AdventOfCode/Day1/Day1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Utils;
+
+namespace AdventOfCode
+{
+    public class Day1
+    {
+        public int Compute(string filePath, int topCount = 1)
+        {
+            var input = FileReader.GetFileContent(filePath).ToList();
+
+            List<int> elfes = new List<int>();
+            int current = 0;
+            foreach (var line in input)
+            {
+                if (line == string.Empty)
+                {
+                    elfes.Add(current);
+                    current = 0;
+                } 
+                else
+                {
+                    current += int.Parse(line);
+                }
+            }
+            elfes.Add(current);
+
+            return elfes.OrderByDescending(x => x).Take(topCount).Sum();
+        }
+
+
+
+    }
+}
