b28ee9f9b14b5a2f473d4027c90566a40d3563c1
maxime.andre <maxime.andr@gmail.com>
2022-12-03 09:48:41 +0100

    day2

---
+++ AdventOfCode/AdventOfCode.Tests/Day2/Day2Tests.cs
@@ -0,0 +1,76 @@
+﻿using System.IO;
+using System.Reflection;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day2Tests
+    {
+        private ITestOutputHelper _output;
+        public Day2Tests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Test1Part1()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day2/Resources/test.txt";
+            Day2 day = new Day2();
+
+            // Act
+            int result = day.Compute(filePath);
+
+            // Assert
+            result.Should().Be(15);
+        }
+
+
+        [Fact]
+        public void SolutionPart1()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day2/Resources/input.txt";
+            Day2 day = new Day2();
+
+            // Act
+            int result = day.Compute(filePath);
+
+            // Result
+            _output.WriteLine(result.ToString());
+        }
+
+        [Fact]
+        public void Test1Part2()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day2/Resources/test.txt";
+            Day2 day = new Day2();
+
+            // Act
+            int result = day.Compute2(filePath);
+
+            // Assert
+            result.Should().Be(12);
+        }
+
+
+        [Fact]
+        public void SolutionPart2()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day2/Resources/input.txt";
+            Day2 day = new Day2();
+
+            // Act
+            int result = day.Compute2(filePath);
+
+            // Result
+            _output.WriteLine(result.ToString());
+        }
+    }
+}
+
---
+++ AdventOfCode/AdventOfCode/Day2/Day2.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Utils;
+
+namespace AdventOfCode
+{
+    public class Day2
+    {
+        public int Compute(string filePath)
+        {
+            var input = FileReader.GetFileContent(filePath).ToList();
+            Dictionary<string, int> combinations = new Dictionary<string, int>()
+            {
+                { "A X", 1 + 3 },
+                { "A Y", 2 + 6 },
+                { "A Z", 3 + 0 },
+                { "B X", 1 + 0 },
+                { "B Y", 2 + 3 },
+                { "B Z", 3 + 6 },
+                { "C X", 1 + 6 },
+                { "C Y", 2 + 0 },
+                { "C Z", 3 + 3 }
+
+            };
+            return input.Select(x => combinations[x]).Sum();
+        }
+
+        public int Compute2(string filePath)
+        {
+            var input = FileReader.GetFileContent(filePath).ToList();
+            Dictionary<string, int> combinations = new Dictionary<string, int>()
+            {
+                { "A X", 3 + 0 },
+                { "A Y", 1 + 3 },
+                { "A Z", 2 + 6 },
+                { "B X", 1 + 0 },
+                { "B Y", 2 + 3 },
+                { "B Z", 3 + 6 },
+                { "C X", 2 + 0 },
+                { "C Y", 3 + 3 },
+                { "C Z", 1 + 6 }
+
+            };
+            return input.Select(x => combinations[x]).Sum();
+        }
+
+
+
+    }
+}
