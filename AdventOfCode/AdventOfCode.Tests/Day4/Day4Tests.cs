09a5775e5a416fdbb7c21e7162a51358d1efbbec
maxime.andre <maxime.andr@gmail.com>
2022-12-04 09:25:09 +0100

    day4

---
+++ AdventOfCode/AdventOfCode.Tests/Day4/Day4Tests.cs
@@ -0,0 +1,76 @@
+﻿using System.IO;
+using System.Reflection;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day4Tests
+    {
+        private ITestOutputHelper _output;
+        public Day4Tests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Test1Part1()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day4/Resources/test.txt";
+            Day4 day = new Day4();
+
+            // Act
+            int result = day.Compute(filePath).includes;
+
+            // Assert
+            result.Should().Be(2);
+        }
+
+
+        [Fact]
+        public void SolutionPart1()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day4/Resources/input.txt";
+            Day4 day = new Day4();
+
+            // Act
+            int result = day.Compute(filePath).includes;
+
+            // Result
+            _output.WriteLine(result.ToString());
+        }
+
+        [Fact]
+        public void Test1Part2()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day4/Resources/test.txt";
+            Day4 day = new Day4();
+
+            // Act
+            int result = day.Compute(filePath).overlaps;
+
+            // Assert
+            result.Should().Be(4);
+        }
+
+
+        [Fact]
+        public void SolutionPart2()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day4/Resources/input.txt";
+            Day4 day = new Day4();
+
+            // Act
+            int result = day.Compute(filePath).overlaps;
+
+            // Result
+            _output.WriteLine(result.ToString());
+        }
+    }
+}
+
---
+++ AdventOfCode/AdventOfCode/Day4/Day4.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Utils;
+
+namespace AdventOfCode
+{
+    public class Day4
+    {
+        public class Elfe
+        {
+            public int Start { get; set; }
+            public int End { get; set; }
+
+            public bool IsIncluding(Elfe elfe)
+            {
+                return elfe.Start >= Start && elfe.End <= End;
+            }
+
+            public bool IsOverlapping(Elfe elfe)
+            {
+                return (elfe.End >= Start && elfe.End <= End) || (elfe.Start >= Start && elfe.Start <= End);
+            }
+        }
+        public (int includes, int overlaps) Compute(string filePath)
+        {
+            var input = FileReader.GetFileContent(filePath).ToList();
+
+            List<(Elfe e1, Elfe e2)> couples = new List<(Elfe e1, Elfe e2)>();
+            foreach(var line in input)
+            {
+                var split = line.Split(",");
+                couples.Add( 
+                    (new Elfe { Start = int.Parse(split[0].Split("-")[0]), End = int.Parse(split[0].Split("-")[1]) },
+                     new Elfe { Start = int.Parse(split[1].Split("-")[0]), End = int.Parse(split[1].Split("-")[1]) })
+                );
+            }
+
+            return (couples.Count(x => x.e1.IsIncluding(x.e2) || x.e2.IsIncluding(x.e1)),
+                    couples.Count(x => x.e1.IsOverlapping(x.e2) || x.e2.IsOverlapping(x.e1)));
+        }
+    }
+}
