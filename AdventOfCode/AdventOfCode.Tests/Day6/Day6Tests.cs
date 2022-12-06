232e0249058b79084f98f8798a067d8c04c87c3f
maxime.andre <maxime.andr@gmail.com>
2022-12-06 09:01:11 +0100

    day6

---
+++ AdventOfCode/AdventOfCode.Tests/Day6/Day6Tests.cs
@@ -0,0 +1,76 @@
+﻿using System.IO;
+using System.Reflection;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day6Tests
+    {
+        private ITestOutputHelper _output;
+        public Day6Tests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Test1Part1()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day6/Resources/test.txt";
+            Day6 day = new Day6();
+
+            // Act
+            int result = day.Compute(filePath);
+
+            // Assert
+            result.Should().Be(7);
+        }
+
+
+        [Fact]
+        public void SolutionPart1()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day6/Resources/input.txt";
+            Day6 day = new Day6();
+
+            // Act
+            int result = day.Compute(filePath);
+
+            // Result
+            _output.WriteLine(result.ToString());
+        }
+
+        [Fact]
+        public void Test1Part2()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day6/Resources/test.txt";
+            Day6 day = new Day6();
+
+            // Act
+            int result = day.Compute(filePath, 14);
+
+            // Assert
+            result.Should().Be(19);
+        }
+
+
+        [Fact]
+        public void SolutionPart2()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day6/Resources/input.txt";
+            Day6 day = new Day6();
+
+            // Act
+            int result = day.Compute(filePath, 14);
+
+            // Result
+            _output.WriteLine(result.ToString());
+        }
+    }
+}
+
---
+++ AdventOfCode/AdventOfCode/Day6/Day6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AdventOfCode.Utils;
+
+namespace AdventOfCode
+{
+    public class Day6
+    {
+        public int Compute(string filePath, int size = 4)
+        {
+            var input = FileReader.GetFileContent(filePath).ToList();
+
+            var splittedLine = input[0].ToCharArray();
+
+            for(int i = 0; i < splittedLine.Length; i++)
+            {
+                if (splittedLine.Where((v, j) => j >= i && j <= i + size - 1).GroupBy(i => i).Count() == size)
+                {
+                    return i + size;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
